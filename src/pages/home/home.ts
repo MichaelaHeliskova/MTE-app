205d920be0be076295cc1c2b7a730c9bc2197402
Michaela Heliskova <misa.heliskova@gmail.com>
2019-01-05 22:29:37 +0100

    Third commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,29 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { WeatherProvider } from '../../providers/weather/weather';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  weather:any;
+  location:{
+    city:string,
+    state:string
+  }
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private weatherProvider: WeatherProvider) {
+  }
+  ionViewWillEnter(){
+    this.location = {
+      city: 'Brno',
+      state: 'Czech Republic'
+    }
 
+    this.weatherProvider.getWeather(this.location.city, this.location.state).subscribe(weather => {
+      console.log(weather);
+    })
   }
 
 }
---
+++ src/providers/weather/weather.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import 'rxjs/add/operator/map';
+
+
+@Injectable()
+export class WeatherProvider {
+  apikey = 'a7a5ec066e425e6b7ad10367d10cac89';
+  url;
+
+  constructor(public http: HttpClient) {
+    console.log('Hello WeatherProvider Provider');
+    this.url = 'http://api.openweathermap.org/data/2.5/weather?q=&APPID='+this.apikey+'';
+  }
+  getWeather(city, state){
+    return this.http.get(this.url+'/'+state+'/'+city)
+    .map(res => res.json());
+  }
+}
